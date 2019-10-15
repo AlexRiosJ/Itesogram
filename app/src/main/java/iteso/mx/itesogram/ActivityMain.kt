5a28cb52dd4276407beaf6a548f6faf88e1d0596
Alejandro Rios Jasso <jassoalex97@hotmail.com>
2019-10-15 08:52:20 -0500

    Finishing Tarea 11

--- app/src/main/java/iteso/mx/itesogram/ActivityMain.kt
+++ app/src/main/java/iteso/mx/itesogram/ActivityMain.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.parse.ParseObject
+import com.parse.ParseQuery
 import iteso.mx.itesogram.adapters.AdapterPhoto
 
 class ActivityMain : AppCompatActivity() {
@@ -13,9 +15,12 @@ class ActivityMain : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val recyclerView = findViewById<RecyclerView>(R.id.activity_main_photos_rv)
-        val photos = arrayListOf<>()
-
-        recyclerView.adapter = AdapterPhoto(photos)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        val query: ParseQuery<ParseObject> = ParseQuery.getQuery("Feed")
+        query.findInBackground { objects, e ->
+            if (e == null) {
+                recyclerView.adapter = AdapterPhoto(objects)
+                recyclerView.layoutManager = LinearLayoutManager(this)
+            }
+        }
     }
 }
--- app/src/main/java/iteso/mx/itesogram/adapters/AdapterPhoto.kt
+++ app/src/main/java/iteso/mx/itesogram/adapters/AdapterPhoto.kt
@@ -6,33 +6,34 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.parse.ParseFile
 import com.parse.ParseObject
 import iteso.mx.itesogram.R
-import kotlinx.android.synthetic.main.activity_login.view.*
 
-class AdapterPhoto (private val names: List<ParseObject>): RecyclerView.Adapter<PhotoViewHolder>() {
+class AdapterPhoto (private val users: List<ParseObject>) : RecyclerView.Adapter<PhotoViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_photo, parent, false)
         return PhotoViewHolder(view)
     }
 
-    override fun getItemCount(): Int = names.size
+    override fun getItemCount(): Int = users.size
 
     override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
-        holder.bind(names[position])
+        holder.bind(users[position])
     }
 }
 
-class PhotoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class PhotoViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
     private val username: TextView = view.findViewById(R.id.item_photo_username_tv)
-    private val photo: ImageView = view.findViewById(R.id.item_photo_photo_iv)
-    private val likes: TextView = view.findViewById(R.id.item_photo_likes_tv)
+    private val image: ImageView = view.findViewById(R.id.item_photo_photo_iv)
+    private val comments: TextView = view.findViewById(R.id.item_photo_likes_tv)
 
     fun bind(user: ParseObject) {
-        var feed = Feed()
-
-        username.text = feed.username
-
+        val parseFile: ParseFile = user.get("photo") as ParseFile
+        Glide.with(view).load(parseFile.url).into(image)
+        username.text = user.get("username") as String
+        comments.text = user.get("comments") as String
     }
 }
